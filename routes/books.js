139b6a1302818af7d483ab604011830ce130e2ed
Ashish Narkhede <ashish@comitydesigns.com>
2016-08-10 22:51:52 -0700

    Adding route for books listing

---
+++ routes/books.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET books listing. */
+router.get('/', function(req, res, next){
+  var books = ['The Alchemist', 'Advanced Javascript for web developers'];
+  res.json(books);
+});
+
+module.exports = router;
